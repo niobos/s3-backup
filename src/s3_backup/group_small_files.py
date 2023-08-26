c5fd51872fe464429ce89596fa97733d1bef0d3e
Niobos <niobos@dest-unreach.be>
2023-08-26 09:55:04 +0200

    Grouping try 3

--- src/s3_backup/group_small_files.py
+++ src/s3_backup/group_small_files.py
@@ -1,4 +1,5 @@
 import dataclasses
+import functools
 import logging
 import os.path
 import typing
@@ -15,14 +16,14 @@ logger = logging.getLogger(__name__)
 class GroupedItem():
     def __init__(self, key: str, underlying: typing.List[BackupItem], size: int = None):
         self._key = key
-        self.underlying = underlying
+        self.underlying_list = underlying
         self.size = size
 
     def key(self) -> str:
         return self._key
 
     def __repr__(self) -> str:
-        return f"<GroupedItem {self._key}: {self.underlying}>"
+        return f"<GroupedItem {self._key}: {self.underlying_list}>"
 
 
 class Tree:
@@ -35,69 +36,80 @@ class Tree:
         def __lt__(self, other):
             return self.key < other.key
 
-    def __init__(self, elements: typing.Iterable[BackupItem]):
-        self.elements = sorted((
-            Tree.Element(e.key(), e.size(), e)
-            for e in elements
-        ))
-        self.key_prefixes = {}  # children
+    def __init__(self):
+        self.elements: typing.List[Tree.Element] = []
+        self.key_prefixes: typing.Dict[str, "Tree"] = {}
 
-    def _split_off_children(self, min_size: int):
-        size = {}
-        char_start = 0
-        previous_first_char = ''
-        i = 0
-        while i < len(self.elements):
-            try:
-                first_char = self.elements[i].key[0]
-            except IndexError:
-                first_char = ''
+    def add_elements(self, elements: typing.Iterable[Element]):
+        for el in elements:
+            self.add_element(el)
 
-            if first_char != previous_first_char:
-                if previous_first_char != '' and \
-                        size[previous_first_char] > min_size:
-                    self._extract_child(slice(char_start, i), previous_first_char)
-                    i = char_start
-
-                previous_first_char = first_char
-                char_start = i
-
-            if first_char not in size:
-                size[first_char] = 0
-            size[first_char] += self.elements[i].size
-
-            i += 1
-
-        if size[previous_first_char] > min_size:
-            self._extract_child(slice(char_start, i), previous_first_char)
-
-        for prefix in list(self.key_prefixes.keys()):
-            child = self.key_prefixes[prefix]
-            child._split_off_children(min_size)
-            if len(child.elements) == 0 and len(child.key_prefixes) == 1:
-                # Pull 1 level up
-                only_prefix = next(iter(child.key_prefixes.keys()))
-                self.key_prefixes[prefix + only_prefix] = child.key_prefixes[only_prefix]
-                del self.key_prefixes[prefix]
-
-    def _extract_child(self, s: slice, prefix: str):
-        child = Tree([])  # Don't use constructor to avoid sorting again
-        prefix_len = len(prefix)
-        for el in self.elements[s]:
-            child.elements.append(Tree.Element(el.key[prefix_len:], el.size, el.item))
-        del self.elements[s]
-        self.key_prefixes[prefix] = child
+    def add_element(self, element: Element):
+        key = element.key
+        if len(key) <= 1:
+            self.elements.append(element)
+        else:
+            first_char = key[0]
+            if first_char not in self.key_prefixes:
+                self.key_prefixes[first_char] = Tree()
+            self.key_prefixes[first_char].add_element(Tree.Element(
+                key=element.key[1:],
+                size=element.size,
+                item=element.item,
+            ))
 
     def elements_size(self) -> int:
         size = 0
-        for el in self.elements:
-            size += el.size
+        for element in self.elements:
+            size += element.size
         return size
+
+    def children_size(self) -> int:
+        size = 0
+        for node in self.key_prefixes.values():
+            size += node.size()
+        return size
+
+    def size(self) -> int:
+        return self.children_size() + self.elements_size()
+
+    def flatten(self):
+        for key_prefix in list(self.key_prefixes.keys()):
+            node = self.key_prefixes[key_prefix]
+            node.flatten()
+
+            if len(node.key_prefixes) == 1 and len(node.elements) == 0:
+                only_prefix = next(iter(node.key_prefixes.keys()))
+                self.key_prefixes[key_prefix + only_prefix] = node.key_prefixes[only_prefix]
+                del self.key_prefixes[key_prefix]
+
+    def merge_min_size(self, min_size: int):
+        for key_prefix in list(self.key_prefixes.keys()):
+            node = self.key_prefixes[key_prefix]
+            node.merge_min_size(min_size)
+
+            if len(node.elements) and node.elements_size() < min_size:
+                # Elements in the node are too small, pull them up to this level
+                self.elements.extend((
+                    Tree.Element(key=key_prefix + el.key, size=el.size, item=el.item)
+                    for el in node.elements
+                ))
+                node.elements = []
+
+            if len(node.elements) == 0 and len(node.key_prefixes) == 0:
+                # Remove empty nodes
+                del self.key_prefixes[key_prefix]
 
 
 def group_files(items: typing.Iterator[BackupItem], min_size: int) -> typing.Iterator[GroupedItem]:
-    items = Tree(items)
-    items._split_off_children(min_size)
+    tree = Tree()
+    tree.add_elements((
+        Tree.Element(item.key(), item.size(), item)
+        for item in items
+    ))
+    tree.flatten()  # reduce recursion
+    tree.merge_min_size(min_size)
+    tree.flatten()  # again, since we pulled up elements in the merge_min_size()-step
 
     def recurse_tree(node: Tree, prefix: str = ''):
         for child_prefix, child in node.key_prefixes.items():
@@ -111,7 +123,7 @@ def group_files(items: typing.Iterator[BackupItem], min_size: int) -> typing.Ite
                 size=node.elements_size(),
             )
 
-    return recurse_tree(items)
+    return recurse_tree(tree)
 
 
 class GroupSmallFilesWrapper(BackupItemWrapper):
@@ -126,6 +138,10 @@ class GroupSmallFilesWrapper(BackupItemWrapper):
         self.small_files_bytes = 0
         self.large_files = 0
         self.large_files_bytes = 0
+        self.suffix = '*~.zip'
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(size_threshold={self.size_threshold})"
 
     def summary(self) -> str:
         return f"{self.small_files} files, {humanize.naturalsize(self.small_files_bytes, binary=True)} " \
@@ -150,7 +166,20 @@ class GroupSmallFilesWrapper(BackupItemWrapper):
             yield entry
 
         logger.info("Would group:")
+        num = 0
+        min_size = None
+        max_size = None
         for entry in group_files(small_files, min_size=self.size_threshold):
-            logger.info(f"{entry.key()}  "
-                        f"({humanize.naturalsize(entry.size, binary=True)})\n    "
-                        + "\n    ".join([str(_) for _ in entry.underlying]))
+            entry._key = entry._key + self.suffix
+            logger.info(f"`{entry.key()}`  "
+                        f"({len(entry.underlying_list)} entries, "
+                        f"{humanize.naturalsize(entry.size, binary=True)})"
+                        "\n    " + "\n    ".join([str(_) for _ in entry.underlying_list]))
+            num += 1
+            if min_size is None or entry.size < min_size:
+                min_size = entry.size
+            if max_size is None or entry.size > max_size:
+                max_size = entry.size
+        logger.info(f"Would have generated {num} ZIPs, ranging from "
+                    f"{humanize.naturalsize(min_size, binary=True)} to "
+                    f"{humanize.naturalsize(max_size, binary=True)}")
