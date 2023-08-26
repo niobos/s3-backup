9752d72aaf115c96ba4e6bd1e322afdc39528ee8
Niobos <niobos@dest-unreach.be>
2023-08-26 09:55:04 +0200

    Sort GroupedItem to avoid unnecessary uploads

--- test/group_small_files_test.py
+++ test/group_small_files_test.py
@@ -1,6 +1,7 @@
 import contextlib
 import datetime
 import io
+import random
 import typing
 import zipfile
 
@@ -9,7 +10,7 @@ from s3_backup.group_small_files import group_files, GroupedItem
 
 
 class MockItem(BackupItem):
-    def __init__(self, key: str, size: int):
+    def __init__(self, key: str, size: int = 1):
         self._key = key
         self._size = size
 
@@ -68,12 +69,18 @@ def test_grouping_2():
 
 
 class MemoryItem(BackupItem):
-    def __init__(self, key: str, content: bytes):
+    def __init__(self, key: str, content: bytes = b'a'):
         self._key = key
         self.content = content
 
+    def __repr__(self) -> str:
+        return f"<MemoryItem {self._key}>"
+
     def key(self) -> str:
         return self._key
+
+    def size(self) -> typing.Optional[int]:
+        return len(self.content)
 
     def mtime(self) -> typing.Optional[float]:
         return datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
@@ -105,3 +112,58 @@ def test_zip():
         assert f.infolist()[0].orig_filename == "foo"
         o = f.read("foo")
         assert o == b"bar"
+
+
+def test_sorting():
+    grouped = list(group_files([
+        MemoryItem("a"),
+        MemoryItem("b"),
+        MemoryItem("c"),
+    ], 2))
+    assert len(grouped) == 1
+    alphabetical = grouped[0]
+    assert len(alphabetical.underlying_list) == 3
+
+    grouped = list(group_files([
+        MemoryItem("b"),
+        MemoryItem("a"),
+        MemoryItem("c"),
+    ], 2))
+    assert len(grouped) == 1
+    shuffled = grouped[0]
+
+    assert shuffled.should_upload(
+        modification_time=datetime.datetime.now() - datetime.timedelta(minutes=1),
+        metadata=alphabetical.metadata(),
+    ) != BackupItem.ShouldUpload.DoUpload
+
+
+def test_sorting_flatten():
+    # Specifically test the sorting when the flattening would change things
+    grouped = list(group_files([
+        MemoryItem("baa"),
+        MemoryItem("bab"),
+        MemoryItem("bac"),
+        MemoryItem("bba"),
+        MemoryItem("bbb"),
+        MemoryItem("ca"),
+        MemoryItem("cb"),
+    ], 3))
+    assert len(grouped) == 2
+    assert len(grouped[0].underlying_list) == 3
+    assert len(grouped[1].underlying_list) == 4
+    previous = grouped[1]
+
+    grouped = list(group_files([
+        MemoryItem("bba"),
+        MemoryItem("bbb"),
+        MemoryItem("ca"),
+        MemoryItem("cb"),
+    ], 3))
+    assert len(grouped) == 1
+    new = grouped[0]
+
+    assert new.should_upload(
+        modification_time=datetime.datetime.now() - datetime.timedelta(minutes=1),
+        metadata=previous.metadata(),
+    ) != BackupItem.ShouldUpload.DoUpload
